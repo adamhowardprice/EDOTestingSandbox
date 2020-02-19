713ed053a1309d84d4365af2b93dd60556c79730
Adam Price <aprice@butterflynetinc.com>
2020-02-19 13:58:34 -0500

    Attempt to write a test that uses EDO

--- TestingSandboxTests/TestingSandboxTests.swift
+++ TestingSandboxTests/TestingSandboxTests.swift
@@ -10,6 +10,16 @@ import XCTest
 @testable import TestingSandbox
 
 class TestingSandboxTests: XCTestCase {
+    
+    @discardableResult
+    func launchAppWithPort(port: Int, value: Int) -> XCUIApplication {
+        let application = XCUIApplication()
+        application.launchArguments = [
+            "-servicePort", String(format: "%d", port), String("-dummyInitValue"),
+            String(format: "%d", value)]
+        application.launch()
+        return application
+    }
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,16 +29,9 @@ class TestingSandboxTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testRemoteInvocation() {
+        launchAppWithPort(port: 1234, value: 10)
+        let service = EDOHostService(port: 2234, rootObject:self, queue:DispatchQueue.main)
     }
 
 }
